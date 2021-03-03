8ba20d0768305f6bdc1d896134ec5df20efa64c1
Sulthan <highlander@highlanders-MacBook-Pro.local>
2021-03-03 23:36:20 +0700

    initial commit

--- app/src/main/java/com/example/sulthan/demobansos/ui/activity/DetailWarungActivity.kt
+++ app/src/main/java/com/example/sulthan/demobansos/ui/activity/DetailWarungActivity.kt
@@ -1,31 +1,29 @@
 package com.example.sulthan.demobansos.ui
 
 import android.content.Intent
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
 import android.view.WindowManager
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.Glide
-import com.example.sulthan.demobansos.MainActivity
 import com.example.sulthan.demobansos.R
+import com.example.sulthan.demobansos.adapter.KategoriAdapter
 import com.example.sulthan.demobansos.adapter.KategoriWarungAdapter
-import com.example.sulthan.demobansos.pojo.Pojo
 import com.example.sulthan.demobansos.pojo.PojoWarung
-import com.example.sulthan.demobansos.ui.dashboard.kategori.BerasFragment
 import kotlinx.android.synthetic.main.activity_detail_warung.*
 import kotlinx.android.synthetic.main.fragment_sarden.*
 import kotlinx.android.synthetic.main.rv_detail.*
+import com.example.sulthan.demobansos.pojo.Pojo
+import com.example.sulthan.demobansos.ui.dashboard.kategori.BerasFragment
+import java.util.ArrayList
 
 class DetailWarungActivity : AppCompatActivity() {
 
-    lateinit var kategoriWarungAdapter: KategoriWarungAdapter
     private lateinit var imageView: ImageButton
+    lateinit var kategoriWarungAdapter: KategoriWarungAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,8 +44,9 @@ class DetailWarungActivity : AppCompatActivity() {
 //            startActivity(Intent(this, BerasFragment::class.java))
 //        }
 
-        setContentView(R.layout.activity_detail_warung)
-        val pojowarung = intent.getParcelableExtra<Pojo.Kategori>("kategori")
+        setContentView(R.layout.rv_detail_warung)
+        val pojoWarung = intent.getParcelableExtra<Pojo.User>("kategori")
+
 
         val listBeras = arrayListOf<PojoWarung> (
             PojoWarung("Pak Sapto","150 KG","1500 M","https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2689&q=80"),
@@ -82,9 +81,22 @@ class DetailWarungActivity : AppCompatActivity() {
             PojoWarung("Sulton","29","1500","https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2689&q=80")
         )
 
-//        rv_main_sarden.apply {
-//
-//        }
+        val kategoriWarungAdapter = KategoriWarungAdapter(listsarden) {
+            pojowarung ->
+            Toast.makeText(this, "clicked ${pojowarung.penjual}", Toast.LENGTH_SHORT).show()
+//            val intent = Intent(this, )
+        }
 
-    }
+        rv_detail_deket.apply {
+            layoutManager = LinearLayoutManager(this@DetailWarungActivity)
+            adapter = kategoriWarungAdapter
+        }
+
+        rv_detail_jauh.apply {
+            layoutManager = LinearLayoutManager(this@DetailWarungActivity)
+            adapter = kategoriWarungAdapter
+        }
+
+        }
+
 }
